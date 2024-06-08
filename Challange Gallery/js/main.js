b65904bd3f89f1a8fdafcc51efcac9713f3386e7
saviomds <dominiquesavio2003@gmail.com>
2024-06-08 15:16:20 +0200

    new updates

--- Challange Gallery/js/main.js
+++ Challange Gallery/js/main.js
@@ -1,13 +1,13 @@
-// JavaScript for zoom effect
-const images = document.querySelectorAll(".image-wrapper img");
+const imageWrappers = document.querySelectorAll(".image-wrapper");
 
-images.forEach((image) => {
+imageWrappers.forEach((wrapper) => {
+  const image = wrapper.querySelector("img");
   // Add event listener for mouseover event
-  image.addEventListener("mouseover", () => {
-    image.style.transform = "scale(1.1)";
+  wrapper.addEventListener("mouseover", () => {
+    wrapper.style.filter = "brightness(0.4) blur(5px)"; // Darken and blur the wrapper
   });
- // Add event listener for mouseout event
-  image.addEventListener("mouseout", () => {
-    image.style.transform = "scale(1)";
+  // Add event listener for mouseout event
+  wrapper.addEventListener("mouseout", () => {
+    wrapper.style.filter = "none"; // Reset filter
   });
 });
